731cc64284f73b70a589a6c9551d1537444c1f37
MorikuMarieke <mariekevdstaaij@gmail.com>
2025-02-12 10:34:28 +0100

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,3 @@
 export const API_BASE = 'https://api.spotify.com/v1/';
 export const NOVI_PLAYGROUND_BACKEND = 'https://api.datavortex.nl/playground/';
 
-// ClientID: 8986546153b549719e7bc9ddf907607f
-// ClientSecret: 8f11e9a8271c4b859abc12d656ba9154
